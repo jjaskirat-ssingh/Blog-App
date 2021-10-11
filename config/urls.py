3988e848aa3e641f9dcc971236d0848f8ab571ff
Jaskirat Singh <72308994+jjaskirat-ssingh@users.noreply.github.com>
2021-10-11 19:22:07 +0530

    User Registration

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('register/', user_views.register, name='register'),
 ]
