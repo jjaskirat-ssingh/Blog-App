27a9eeeebf29c5334148669ee4083de9aa1bb61a
Jaskirat Singh <72308994+jjaskirat-ssingh@users.noreply.github.com>
2021-10-11 12:04:56 +0530

    Added templates

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author': 'Jaskirat',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 10, 2021'
+    },
+    {
+        'author': 'Random',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 11, 2021'
+    }
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
